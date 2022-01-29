1d8266ece23f9176b7c25d4eaf5c91b01c1b34f7
nichojohnson84 <nickcjohnson84@outlook.com>
2022-01-29 09:55:21 -0700

    trying to get javeN and link updated

---
+++ assets/js/scriptN.js
@@ -0,0 +1,35 @@
+var repoList = document.querySelector('ul');
+var fetchButton = document.getElementById('fetch-button');
+
+function getApi() {
+  // Replace `old list with new one upon reload` with anyone else's GitHub username
+  var requestUrl = 'https://themealdb.p.rapidapi.com/randomselection.php';
+
+
+/*const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://themealdb.p.rapidapi.com/randomselection.php",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "themealdb.p.rapidapi.com",
+		"x-rapidapi-key": "74b0896eedmsha54b2117250ace6p1d5405jsn0bb56ec209ae"
+	}
+};
+
+/*Code Snippets
+Example Responses
+Results*/
+fetch("https://themealdb.p.rapidapi.com/randomselection.php", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "themealdb.p.rapidapi.com",
+		"x-rapidapi-key": "74b0896eedmsha54b2117250ace6p1d5405jsn0bb56ec209ae"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
