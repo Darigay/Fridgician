0dc9a3c1cdc5b724e232d6c72f851ca7e209257e
nichojohnson84 <nickcjohnson84@outlook.com>
2022-01-29 11:23:11 -0700

    nichoscript

---
+++ assets/js/scriptN.js
@@ -0,0 +1,44 @@
+/*var repoList = document.querySelector('ul');
+var fetchButton = document.getElementById('fetch-button');
+
+function getApi() {
+  // Replace `old list with new one upon reload` with anyone else's GitHub username
+  var requestUrl = 'https://themealdb.p.rapidapi.com/randomselection.php';*/
+
+
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://themealdb.p.rapidapi.com/randomselection.php",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "themealdb.p.rapidapi.com",
+		"x-rapidapi-key": "74b0896eedmsha54b2117250ace6p1d5405jsn0bb56ec209ae"
+	}
+};
+
+/*Code Snippets
+Example 
+Results*/
+fetch("https://themealdb.p.rapidapi.com/randomselection.php", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "themealdb.p.rapidapi.com",
+		"x-rapidapi-key": "74b0896eedmsha54b2117250ace6p1d5405jsn0bb56ec209ae"
+	}
+})
+.then(function (response) {
+	if (response.ok) {
+		return response.json()
+	}
+})
+.then(function (data) {
+	//randomMeal (data.meals)
+	console.log(data.meals)
+});
+//var randomMeal = function(data.meals){
+	//for (let i = 0; i < choices.length; i++)
+	categoryelement.textcontent = data.meals.category
+
+//}
+//
