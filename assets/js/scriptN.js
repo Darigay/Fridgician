bd818891ecaafe88cde9311cd66a91bb31fb3868
Aaron Jones <ernypoo92@gmail.com>
2022-02-02 01:36:32 -0700

    js is stupid

--- assets/js/scriptN.js
+++ assets/js/scriptN.js
@@ -39,11 +39,12 @@ const settings = {
       var strMeal = data.meals[i].strMeal;
       var strCategory = data.meals[i].strCategory;
       var strSource = data.meals[i].strSource;
+      var strTags = data.meals[i].strTags;
       
       strMealThumb.toString();
       
 
-      console.log (strMeal, strMealThumb, strCategory, strSource)
+      console.log (strMeal, strMealThumb, strCategory, strSource, strTags)
       // Card Big Image
       var cardImg = document.createElement("div");
         cardImg.setAttribute ("class", "card-image is-hidden-mobile");
@@ -56,16 +57,22 @@ const settings = {
         cardContent.setAttribute ("class", "media-left is-hidden-tablet");
         cardContent.appendChild(cardContainer);
       
+        //Recipe Small Image
         var cardSmallImg = document.createElement("div")
           cardSmallImg.setAttribute ("class", "image is-96x96");
           cardSmallImg.innerHTML = "<figure class='image is-96x96'><img src="+strMealThumb+" alt='image of "+strMeal+"'></figure>";
           cardSmallImg.appendChild(cardContent);
 
+        //Recipe Title and Category
         var recipeName = document.createElement("div")
           recipeName.setAttribute ("class", "media-content");
           recipeName.innerHTML = "<h4 class='title is-3'>"+strMeal+"</h4><p class='subtitle is-6'>'Catagory: "+strCategory+"'</p>";
           recipeName.appendChild(cardContent);
-    
+      
+      var recipeButton = document.createElement("div")
+      recipeButton.setAttribute ("class", "content");
+      recipeButton.innerHTML = "Recipe Tags <a>"+strTags+"</a>.<br><button class='button is-dark is-rounded'>View Recipe</button>";
+      recipeButton.appendChild(cardContainer);
     
 		cardsContainerEl.append(cardContainer);
     
