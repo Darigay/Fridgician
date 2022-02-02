74b026647488a08b29f445741050f6409fdc8061
Aaron Jones <ernypoo92@gmail.com>
2022-02-02 01:02:17 -0700

    I'm so close

--- assets/js/scriptN.js
+++ assets/js/scriptN.js
@@ -24,32 +24,50 @@ const settings = {
 	.then(function (data) {
 	  //randomMeal (data.meals)
 	  console.log(data.meals);
+
 	  const cardsContainerEl = document.querySelector(".cards-container");
 	  console.log(cardsContainerEl);
+
 	  for (var i = 0; i < data.meals.length; i++) {
-		  console.log("test");
-		var cardCont = document.createElement("div");
-		  cardCont.setAttribute('class',"card-columns is-3")
+		  console.log(data.meals[i].strMeal);
+		var cardContainer = document.createElement("div");
+		  cardContainer.setAttribute('class',"card column is-3-desktop is-4-tablet is")
     
       
       //need right tags
-      var strMealThumb = data.meals[i].strMealThumb ;
+      var strMealThumb = data.meals[i].strMealThumb;
       var strMeal = data.meals[i].strMeal;
       var strCategory = data.meals[i].strCategory;
       var strSource = data.meals[i].strSource;
       
-      console.log (strMeal, strMealThumb, strCategory, strSource)
+      strMealThumb.toString();
+      
 
+      console.log (strMeal, strMealThumb, strCategory, strSource)
+      // Card Big Image
       var cardImg = document.createElement("div");
-        cardImg.setAttribute ("class", "card-image");
+        cardImg.setAttribute ("class", "card-image is-hidden-mobile");
         cardImg.innerHTML = "<figure class='image is-1by1'><img src="+strMealThumb+" alt='image of " + strMeal +"'></figure>";
-        cardImg.appendChild(cardCont);
+        cardImg.appendChild(cardContainer);
         console.log (strMealThumb)
 
+      //Card Content
+      var cardContent = document.createElement("div")
+        cardContent.setAttribute ("class", "media-left is-hidden-tablet");
+        cardContent.appendChild(cardContainer);
+      
+        var cardSmallImg = document.createElement("div")
+          cardSmallImg.setAttribute ("class", "image is-96x96");
+          cardSmallImg.innerHTML = "<figure class='image is-96x96'><img src="+strMealThumb+" alt='image of "+strMeal+"'></figure>";
+          cardSmallImg.appendChild(cardContent);
 
-    //divEl.innerHTML = "<div class='card-content'><div class='media'><div class='media-content'><p class='title is-3'>" + strMeal + "</p><p class='subtitle is-6'>" + strCategory + "</p></div></div><div class='content'><a>@bulmaio</a>.<a href='#''>#css</a> <a href='#''>#responsive</a><br><time datetime='2016-1-1'>11:09 PM - 1 Jan 2016</time></div></div>";
+        var recipeName = document.createElement("div")
+          recipeName.setAttribute ("class", "media-content");
+          recipeName.innerHTML = "<h4 class='title is-3'>"+strMeal+"</h4><p class='subtitle is-6'>'Catagory: "+strCategory+"'</p>";
+          recipeName.appendChild(cardContent);
     
     
-		//cardsContainerEl.appendChild(divEl);
+		cardsContainerEl.append(cardContainer);
+    
   }
 	});
